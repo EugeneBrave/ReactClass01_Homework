445311470d07bf2c7105dd95c6006c3e590cceed
Eugene <Eugene@EugeneMac.local>
2017-06-04 17:13:08 +0800

    split component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import Block from './Block';
 
 class App extends Component {
     constructor(props) {
@@ -53,54 +54,5 @@ class App extends Component {
     }
 }
 
-var blockStyle = {
-  color: 'white',
-  backgroundColor: 'blue',
-  display: 'inline',
-  float: 'left',
-  width: '50px',
-  height: '50px',
-  font: 'bold 18pt arial',
-  margin: '5px 5px 5px 5px',
-  lineHeight: '50px',
-  textAlign: 'center',
-  cursor: 'pointer'
-};
 
-var blockStyle2 = {
-  color: 'white',
-  backgroundColor: 'red',
-  display: 'inline',
-  float: 'left',
-  width: '50px',
-  height: '50px',
-  font: 'bold 18pt arial',
-  margin: '5px 5px 5px 5px',
-  lineHeight: '50px',
-  textAlign: 'center',
-  cursor: 'pointer'
-};
-
-var blockStyle3 = {
-  color: 'white',
-  backgroundColor: 'blue',
-  display: 'inline',
-  float: 'left',
-  width: '110px',
-  height: '50px',
-  font: 'bold 18pt arial',
-  margin: '5px 5px 5px 5px',
-  lineHeight: '50px',
-  textAlign: 'center',
-  cursor: 'pointer'
-};
-
-class Block extends Component {
-    render() {
-        var style = parseInt(this.props.value) ? blockStyle : this.props.value == 0 ? blockStyle3 : blockStyle2;
-        return (
-            <div className="number" style={style} onClick={(e) => this.props.handleClick(this.props.value,e)}>{this.props.value}</div>
-        )
-    }
-}
 module.exports = App;
